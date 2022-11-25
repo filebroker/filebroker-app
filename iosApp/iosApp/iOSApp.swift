b3c88a66d6381d01723af5d490bc857cb9973f70
Robin Friedli <robinfriedli@icloud.com>
2022-11-25 15:37:30 +0100

    add NavigationView to iOS app

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -2,9 +2,29 @@ import SwiftUI
 
 @main
 struct iOSApp: App {
-	var body: some Scene {
-		WindowGroup {
-			ContentView()
-		}
-	}
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView().navigationTitle("filebroker")
+            }
+        }
+    }
+}
+
+struct MenuView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                Image(systemName: "home")
+                    .foregroundColor(.white)
+                    .imageScale(.large)
+                Text("Home")
+                    .foregroundColor(.white)
+                    .font(.headline)
+            }
+            .padding(.top, 100)
+        }
+        .padding()
+        .frame(maxWidth: .infinity, alignment: .leading)
+    }
 }
