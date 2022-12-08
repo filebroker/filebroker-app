50fa623c46f98d4c0643008451fad7e24149e613
Robin Friedli <robinfriedli@icloud.com>
2022-12-08 23:43:29 +0100

    implement logout for iOS

--- iosApp/iosApp/ProfileView.swift
+++ iosApp/iosApp/ProfileView.swift
@@ -13,11 +13,18 @@ struct ProfileView: View {
     
     @ObservedObject var env: Env
     
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    
     var body: some View {
         if let login = env.api.currentLogin {
             VStack {
                 Text("Welcome, you are logged in as")
                 Text(login.user.user_name)
+                Spacer()
+                Button("Logout", role: .destructive) {
+                    env.api.currentLogin = nil
+                    self.presentationMode.wrappedValue.dismiss()
+                }
             }
         } else {
             Text("Not logged in")
