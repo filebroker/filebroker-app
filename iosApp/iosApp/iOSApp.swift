23a12d3b5c6f5362e6133d246193731f7b406f7e
Robin Friedli <robinfriedli@icloud.com>
2022-12-08 23:29:34 +0100

    implement persistent logins for iOS
     - store refresh tokens using UserDefaults and attempt to refresh the
       login on startup using the persisted refresh token

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,3 +1,4 @@
+import os
 import SwiftUI
 import shared
 
@@ -9,6 +10,16 @@ struct iOSApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView(env: env)
+                .onAppear {
+                    let refreshToken = UserDefaults().string(forKey: "refresh_token")
+                    if refreshToken != nil {
+                        env.api.refreshLogin(refreshToken: refreshToken!) { login, error in
+                            if let error = error {
+                                Logger().error("Failed to refresh login with error \(error.localizedDescription)")
+                            }
+                        }
+                    }
+                }
         }
     }
 }
@@ -21,6 +32,11 @@ class Env : ObservableObject {
         api.loginChangeCallback = { login in
             DispatchQueue.main.async {
                 self.objectWillChange.send()
+                if login != nil {
+                    UserDefaults().set(login!.refreshToken, forKey: "refresh_token")
+                } else {
+                    UserDefaults().removeObject(forKey: "refresh_token")
+                }
             }
         }
     }
